8350b9034213ab362298d14f95405ae778fa883a
Mehrab Bozorgi <mehrab.bozorgi@gmail.com>
2022-07-10 23:20:55 +0430

    Real State

---
+++ lib/theme/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const backGroundColor=Color(0xFF1F1C2D);
+const darkColor=Color(0xFF242635);
+const buttonColor1=Color(0xFF3269F2);
+const buttonColor2=Color(0xFF79B2FD);
